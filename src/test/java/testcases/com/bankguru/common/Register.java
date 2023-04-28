4e2896e45a6258c6bcf67fa56eda0f948dc6e98a
Tu Thuc <tuthuc176216@gmail.com>
2023-04-28 17:38:36 +0700

    update testcase

---
+++ src/test/java/testcases/com/bankguru/common/Register.java
@@ -0,0 +1,45 @@
+package testcases.com.bankguru.common;
+
+import commons.BaseTest;
+import commons.Environment;
+import commons.PageGeneratorManager;
+import org.aeonbits.owner.ConfigFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.*;
+import pageObjects.LoginPageObject;
+import pageObjects.RegisterPageObject;
+import utilities.DataHelper;
+
+public class Register extends BaseTest {
+
+    @Parameters({"envName", "serverName", "browserName", "ipAddress", "port", "osName", "osVersion"})
+    @BeforeTest
+    public void beforeTest(@Optional("local") String envName, @Optional("dev") String serverName, @Optional("chrome") String browserName, @Optional("Windows") String osName, @Optional("10") String osVersion,
+                            @Optional("localhost") String ipAddress, @Optional("4444") String portNumber) {
+       String env = System.getProperty("env");
+        ConfigFactory.setProperty("server", env);
+        environment = ConfigFactory.create(Environment.class);
+        driver = getBrowserDriver(envName, browserName, environment.getUserUrl(), ipAddress, portNumber, osName, osVersion);
+        dataFaker = DataHelper.getDataHelper();
+        loginPage = PageGeneratorManager.getLoginPage(driver);
+
+        registerPage = loginPage.clickToHereLink();
+
+        registerPage.inputToEmailAdressTextbox(dataFaker.getEmailAddress());
+
+        registerPage.clickToSubmitButton();
+
+        userID = registerPage.getUserInfor();
+
+        password = registerPage.getPasswordInfor();
+
+        closeBrowserAndDriver("envName");
+    }
+    private WebDriver driver;
+    private DataHelper dataFaker;
+    public static String userID, password;
+    private LoginPageObject loginPage;
+    private RegisterPageObject registerPage;
+    private Environment environment;
+
+}
